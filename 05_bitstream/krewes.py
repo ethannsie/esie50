3d9890f099bb53855ba904bdbb84e0c2a443fc55
ethannsie <esie50@stuy.edu>
2024-09-17 21:52:00 -0400

    actually finished 05

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -7,15 +7,22 @@ SoftDev
 time spent: 0.5
 """
 
+import random
+
 f = open("krewes.txt", "r")
 people = f.read().replace("\n", "").split('@@@')
 for count, person in enumerate(people):
     people[count] = person.split('$$$')
     people[count] = {"period":people[count][0], "devo":people[count][1], "ducky":people[count][2]}
 
+randoDevo = random.randint(0, len(people)-1)
+
+print("Name: " + people[randoDevo].get("devo") + " | Period: " + people[randoDevo].get("period") + " | Ducky: " + people[randoDevo].get("ducky"))
+
+"""
 print(people)
 
-# Lets say you want to know all of the people/duckys in period 4
+#Lets say you want to know all of the people/duckys in period 4
 p4Peeps = []
 p4Duckys = []
 for person in people:
@@ -24,4 +31,4 @@ for person in people:
         p4Duckys.append(person.get("ducky"))
 print(p4Peeps)
 print(p4Duckys)
-
+"""
