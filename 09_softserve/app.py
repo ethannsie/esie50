703bfe2fd3edb6fd6a0b3e09be082da96e181c6c
ethannsie <esie50@stuy.edu>
2024-09-25 09:00:38 -0400

    commented app.py

--- 09_softserve/app.py
+++ 09_softserve/app.py
@@ -1,6 +1,10 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# September 2024
+'''Ethan Sie, Marco Quintero, Ankita Saha, Colyi Chen
+  Trojan Horses
+  SoftDev
+  printing occupations.csv information into the flask server!
+  2024-9-23
+  time spent: 0.6 hours
+  '''
 
 from flask import Flask
 import csv
@@ -13,7 +17,7 @@ def fileParser(file): #reads csv
         csvFile = csv.reader(csvfile, delimiter='\n')
         data = []
         for line in csvFile:
-            data.append(line)
+            data.append(line) # Puts all of the data from the csv into the list
     return data
 
 def splitHeaders(dataSet): #converts data to dictionary
@@ -22,7 +26,7 @@ def splitHeaders(dataSet): #converts data to dictionary
         for string in data:
             for count, letter in enumerate(string[:len(string)-1]):
                 if letter == ',' and string[count+1].isnumeric():
-                    dictValues[string[:count]] = float(string[count+1:])
+                    dictValues[string[:count]] = float(string[count+1:]) #Takes each input and enters it as a Job:Percentage dictionary
     return dictValues
 
 def randomizeJob(dict): #randomizes job, weighted
@@ -43,15 +47,16 @@ CSV_FILE_PATH = 'occupations.csv'
 def hello_world():
     #print("the __name__ of this module is... ")
     #print(__name__)
-    jobData = fileParser(CSV_FILE_PATH)
-    headers = jobData[0][0].split(',')
-    numJobData = jobData[1:len(jobData)-1]
-    dictValues = splitHeaders(numJobData)
-    randJob = randomizeJob(dictValues)
-    jobList = "<table><tr><th>Job List</th></tr>"
+    jobData = fileParser(CSV_FILE_PATH) #Parses csv data
+    headers = jobData[0][0].split(',') #Initial delimit
+    numJobData = jobData[1:len(jobData)-1] #Cuts out the header and "Total"
+    dictValues = splitHeaders(numJobData) #Converts into dictinoary
+    randJob = randomizeJob(dictValues) #Finds random job
+    jobList = "<table><tr><th>Job List</th></tr>" 
     for job in dictValues.keys():
-        jobList += "<tr><td>" + job + "</td></tr>"
+        jobList += "<tr><td>" + job + "</td></tr>" #html styling for job list table
     return "<head><style>table, th, td {border: 1px solid black;}</style></head><body><h1>Trojan Horses: Ethan Sie, Ankita Saha, Marco Quintero, Colyi Chen</h1>Randomly Chosen Job: " + randJob + "<br><br>" + jobList + "</table>"
+    #returns styling to be printed on to the flask server
 
 if __name__ == "__main__":      # true if this file NOT imported
     app.debug = True            # enable auto-reload upon code change; We found this to be true and also that the debugger will print to the terminal that the code has been changed
