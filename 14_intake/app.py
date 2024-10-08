24d85c3f7ba15099a1672565e96cacbace7f19f4
ethannsie <esie50@stuy.edu>
2024-10-08 09:14:03 -0400

    changes

--- 14_intake/app.py
+++ 14_intake/app.py
@@ -45,17 +45,17 @@ PROTIP: Insert your own in-line comments
 
 @app.route("/") #, methods=['GET', 'POST']) # Flask defaults to GET (shown in flask terminal on page load); POST likely used to submit things back to the server (as GET is used to retrieve things from the server) -> Can be used to give data from a form to the server to use in a HTML document
 def disp_loginpage():
-    # print("\n\n\n")
-    # print("***DIAG: this Flask obj ***")
-    # print(app) # Prints <Flask 'app'>
-    # print("***DIAG: request obj ***")
-    # print(request) # Prints <Request 'http://127.0.0.1:5000/' [GET]> 127.0.0.1 - - [07/Oct/2024 21:04:20] "GET / HTTP/1.1" 200 -
-    # print("***DIAG: request.args ***")
-    #print(request.args) # Prints ImmutableMultiDict([]) => Datatype of previous print(request), dosen't have anything inside because no args
-    # print("***DIAG: request.args['username']  ***")
-    # print(request.args['username']) # Leads to a BadRequestKeyError likely due to the lack of a username query paramater in the URL => Confirmed, When placing ?username=J into the URL, Code functions as normal and terminal prints J (the username)
-    # print("***DIAG: request.headers ***")
-    # print(request.headers) # Prints information about the browser, host, connection type, system and some more cryptic things
+#    print("\n\n\n")
+#    print("***DIAG: this Flask obj ***")
+#    print(app) # Prints <Flask 'app'>
+#     print("***DIAG: request obj ***")
+#     print(request) # Prints <Request 'http://127.0.0.1:5000/' [GET]> 127.0.0.1 - - [07/Oct/2024 21:04:20] "GET / HTTP/1.1" 200 -
+#     print("***DIAG: request.args ***")
+#     print(request.args) # Prints ImmutableMultiDict([]) => Datatype of previous print(request), dosen't have anything inside because no args
+#    print("***DIAG: request.args['username']  ***")
+#    print(request.args['username']) # Leads to a BadRequestKeyError likely due to the lack of a username query paramater in the URL => Confirmed, When placing ?username=J into the URL, Code functions as normal and terminal prints J (the username)
+#     print("***DIAG: request.headers ***")
+#     print(request.headers) # Prints information about the browser, host, connection type, system and some more cryptic things
     return render_template( 'login.html' )
 
 # When visitng this page through the URL (host/auth), it returns errors when requesting the username, as it is not given (similar to the index)
@@ -68,10 +68,10 @@ def authenticate():
     # print(request) # Returns the URL with the same GET request
     # print("***DIAG: request.args ***")
     # print(request.args) # Returns the ImmutableMultiDict with the username and username value sin one list, and the input submit type of the HTML and its corresponding name
-    # print("***DIAG: request.args['username']  ***")
-    # print(request.args['username']) # Shows the username
-    # print("***DIAG: request.headers ***")
-    # print(request.headers) # Shows the same information as before
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username']) # Shows the username
+    #print("***DIAG: request.headers ***")
+    #print(request.headers) # Shows the same information as before
 
     # When this functiaonlity is added, it makes the auth page more personalized for users, but also increases the vulnerabilities as nothing sanitizes the username input
     username = request.args.get('username') # Considers the empty box an empty string? => Works Regardless of what is submitted
