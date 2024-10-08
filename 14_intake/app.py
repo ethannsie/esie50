6b296143da38f243c1ba2073353ae805ec7be060
ethannsie <esie50@stuy.edu>
2024-10-08 00:36:05 -0400

    done

--- 14_intake/app.py
+++ 14_intake/app.py
@@ -1,6 +1,10 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# October 2024
+"""
+Elmos_Cheez-its: Amanda Tan, Ethan Sie, Aidan Wong
+SoftDev
+K14 - User Input with Flask
+2024-10-07
+time spent: 0.7
+"""
 
 # import conventions:
 # list most general first (standard python library)
@@ -39,36 +43,39 @@ PROTIP: Insert your own in-line comments
    understand what is going on.
 '''
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/") #, methods=['GET', 'POST']) # Flask defaults to GET (shown in flask terminal on page load); POST likely used to submit things back to the server (as GET is used to retrieve things from the server) -> Can be used to give data from a form to the server to use in a HTML document
 def disp_loginpage():
-    #print("\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-    #print("***DIAG: request obj ***")
-    #print(request)
-    #print("***DIAG: request.args ***")
-    #print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    #print("***DIAG: request.headers ***")
-    #print(request.headers)
+    # print("\n\n\n")
+    # print("***DIAG: this Flask obj ***")
+    # print(app) # Prints <Flask 'app'>
+    # print("***DIAG: request obj ***")
+    # print(request) # Prints <Request 'http://127.0.0.1:5000/' [GET]> 127.0.0.1 - - [07/Oct/2024 21:04:20] "GET / HTTP/1.1" 200 -
+    # print("***DIAG: request.args ***")
+    #print(request.args) # Prints ImmutableMultiDict([]) => Datatype of previous print(request), dosen't have anything inside because no args
+    # print("***DIAG: request.args['username']  ***")
+    # print(request.args['username']) # Leads to a BadRequestKeyError likely due to the lack of a username query paramater in the URL => Confirmed, When placing ?username=J into the URL, Code functions as normal and terminal prints J (the username)
+    # print("***DIAG: request.headers ***")
+    # print(request.headers) # Prints information about the browser, host, connection type, system and some more cryptic things
     return render_template( 'login.html' )
 
-
+# When visitng this page through the URL (host/auth), it returns errors when requesting the username, as it is not given (similar to the index)
 @app.route("/auth") # , methods=['GET', 'POST'])
 def authenticate():
-    #print("\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-    #print("***DIAG: request obj ***")
-    #print(request)
-    #print("***DIAG: request.args ***")
-    #print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    #print("***DIAG: request.headers ***")
-    #print(request.headers)
-    return "Waaaa hooo HAAAH"  #response to a form submission
+    # print("\n\n\n")
+    # print("***DIAG: this Flask obj ***")
+    # print(app) # Same <Flask 'app'>
+    # print("***DIAG: request obj ***")
+    # print(request) # Returns the URL with the same GET request
+    # print("***DIAG: request.args ***")
+    # print(request.args) # Returns the ImmutableMultiDict with the username and username value sin one list, and the input submit type of the HTML and its corresponding name
+    # print("***DIAG: request.args['username']  ***")
+    # print(request.args['username']) # Shows the username
+    # print("***DIAG: request.headers ***")
+    # print(request.headers) # Shows the same information as before
+
+    # When this functiaonlity is added, it makes the auth page more personalized for users, but also increases the vulnerabilities as nothing sanitizes the username input
+    username = request.args.get('username') # Considers the empty box an empty string? => Works Regardless of what is submitted
+    return f"Waaaa hooo HAAAH {username}"  #response to a form submission
 
 
     
--- 14_intake/testmod0.py
+++ 14_intake/testmod0.py
@@ -1,11 +1,20 @@
+"""
+Elmos_Cheez-its: Amanda Tan, Ethan Sie, Aidan Wong
+SoftDev
+K14 - User Input with Flask
+2024-10-07
+time spent: 0.7
+"""
+
 def goo():
     return "this is return string from goo() in test module"
 
+# Print statements appear regardless of which python file is run using flask, Only appears on first run (dosen't repeat upon page refresh)
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("this print statement came from test module")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-
+# Printed when this python file is run using flask (through python testmod0.py)
 if __name__ == "__main__": #false if this file imported as module
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("this CONDITIONAL print statement came from test module")
