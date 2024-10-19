c9ad70d15abc0cc77f9bf0965c434960757130f6
ethannsie <esie50@stuy.edu>
2024-10-19 18:37:06 -0400

    done 19

--- 19_csv2db/build_db.py
+++ 19_csv2db/build_db.py
@@ -1,7 +1,10 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev
-#skeleton/stub :: SQLITE3 BASICS
-#Oct 2024
+"""
+Elmos_Cheez-its: Amanda Tan, Ethan Sie, Aidan Wong
+SoftDev
+K19 - Creating databases with SQL
+2024-10-18
+time spent: 1.0
+"""
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -12,21 +15,34 @@ DB_FILE="discobandit.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
-#=========================================================
-#c.execute("CREATE TABLE amunals (name TEXT, id INTEGER PRIMARY KEY);")
-SELECT * FROM amunals;
+#==========================================================
+
 
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-CREATE TABLE amunals (name TEXT, id INTEGER PRIMARY KEY);
-INSERT INTO amunals VALUES ("whose-it", 2);
-INSERT INTO amunals VALUES ("whats-it", 1);
-INSERT INTO amunals VALUES ("blurb", 0);
+# This code is very static, and relies on changing many aspects for new tables
+
+c.execute('CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)') # Command creates a table called courses with the columns code, mark, and id
+
+with open('courses.csv', 'r') as file: # Opens the courses.csv file to read it
+    reader = csv.DictReader(file) # Sets variable named reader to DictReader, which will allow the iteration of the csv file as dictionaries for each row
+    for _ in reader: # NOTE: The DictReader object automatically uses the first line of the CSV to generate the keys (the columns), so it does not need to be skipped
+        c.execute('INSERT INTO courses VALUES (?, ?, ?);', (_.get('code'), _.get('mark'), _.get('id'))) # Inserts the values from each dictionary returned from the reader into the table
+
+# Same process as above, but for the students.csv file
+c.execute('CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)')
+
+with open('students.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for _ in reader:
+        c.execute('INSERT INTO students VALUES (?, ?, ?);', (_.get('name'), _.get('age'), _.get('id')))
+
+#command = ""          # test SQL stmt in sqlite3 shell, save as string
+#c.execute(command)    # run SQL statement
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+
 
 #==========================================================
 
