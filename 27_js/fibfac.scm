75840a4c95940573f5b334597c274690dd0c64d6
Ethan Sie <144497224+ethannsie@users.noreply.github.com>
2025-01-07 00:00:17 -0500

    Update fibfac.scm

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -1,3 +1,10 @@
+; Tanzeem Hasan, Ethan Sie, Brian Liu
+; Chewy_Chupucabras
+; SoftDev
+; K27: JS is just Scheme with Java syntax
+; 2025-01-06
+; time spent: 10 mins
+  
 (define (fib n)
   (if (<= n 1)
       n
